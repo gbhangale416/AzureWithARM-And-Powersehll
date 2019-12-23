e70bb9f97ae6d1608933de7e27e300f514582ebf
Girish Bhangale <gbhangale416@gmail.com>
2019-12-23 11:36:07 +0530

    Added powershell script to Create VirtualMachine With Default Parameters for given resourcegroup and subnet

---
+++ Create-VirtualMachine-With-Default-Parameters.ps1
@@ -0,0 +1,5 @@
+﻿# Create Virtual Machine
+
+New-AzureRmVM -ResourceGroupName prod -Name webserver -Location 'East US' `
+-VirtualNetworkName vnet_prod -SubnetName Healthcare -SecurityGroupName rdpallow `
+-PublicIpAddressName randompublicip -OpenPorts 80,3389
